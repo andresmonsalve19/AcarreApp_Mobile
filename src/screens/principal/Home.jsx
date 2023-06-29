aba9aed5d932a9eba1bc7a1fdfbf95249e12f9ed
wgarciab <55595765+wgarciab@users.noreply.github.com>
2023-06-29 16:20:06 -0500

    Add sign in alert, sign up alert, log out button

--- src/screens/principal/Home.jsx
+++ src/screens/principal/Home.jsx
@@ -3,6 +3,8 @@ import {StyleSheet, Text, View, Image, ScrollView} from "react-native";
 import Colors from "../../constants/Colors";
 import CustomTitle from "../../components/CustomTitle";
 import GetStartedButton from "../../components/home/GetStartedButton";
+import LogoutButton from "../../components/home/LogoutButton";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const image = require("../../../assets/acarreapp_icon.png");
 
@@ -11,11 +13,17 @@ export default function HomeScreen({navigation}) {
         navigation.navigate("Contrata");
     };
 
+    const onPressLogOutButton = async() => {
+        await AsyncStorage.removeItem('token');
+        navigation.navigate("Signin");
+    };
+
     return (
         <ScrollView contentContainerStyle={{alignItems: "center"}}>
             <Image style={styles.logo} resizeMode="contain" source={image} />
             <CustomTitle title="Bienvenido a AcarreApp!" />
             <GetStartedButton navigateTo={goToRequestAcarreo} title="Comenzar" />
+            <LogoutButton title="Cerrar sesión" onPress={onPressLogOutButton} />
         </ScrollView>
     );
 }
